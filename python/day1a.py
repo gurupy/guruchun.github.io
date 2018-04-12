1e0fe2629dd2b5ee2ce37befb934bbfa12207b07
chunhc <guruchun.us@gmail.com>
2018-04-12 16:07:11 +0900

    remove python sources, add new article

--- python/day1a.py
+++
@@ -1,6 +0,0 @@
-h = int(input("input height of tree: "))
-for i in range(h):
-  print("{0:^{width}}".format("*" * (i*2+1), width=h*2))
-for i in range(int(h/3)):
-  w = h//10
-  print("{0:^{width}}".format("*" * (w*2+1), width=h*2))
